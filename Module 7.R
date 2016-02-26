2128feb554d063de816c81269e3ebe8899e7adc8
meredithakrause <meredithk@mail.usf.edu>
2016-02-26 11:53:52 -0500

    Create Module 7.R

---
+++ Module 7.R
@@ -0,0 +1,7 @@
+beaver1
+plot(beaver1)
+summary(beaver1)
+print(beaver1)
+!isS4(beaver1) #is this object S3?
+!is(beaver1, "refClass") #is this object S4?
+!is.object(beaver1) #is this object a base object?
